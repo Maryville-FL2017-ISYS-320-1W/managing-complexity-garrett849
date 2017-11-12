23cecd40f48e60b185174ef67c9491b50165de1f
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-11 21:05:46 -0600

    Outputloop

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -1,24 +1,20 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Garrett Weissenborn
+  	Date: 11/3/2017
 */
-
-
 
 public class P1_OutputLoopResult {
 
 	public static void main(String[] args) {
 		printSumOfSquaresTo5();
 	}
-	
+
 	public static void printSumOfSquaresTo5() {
-		for( int i = 1; i <= 5; i++ ) {
+		for (int i = 1; i <= 5; i++) {
 			int finalResult = 0;
 			finalResult = finalResult + i * i;
+			System.out.println("Final result is: " + finalResult);
 		}
-		
-		System.out.println( "Final result is: " + finalResult );
 	}
-
 }
