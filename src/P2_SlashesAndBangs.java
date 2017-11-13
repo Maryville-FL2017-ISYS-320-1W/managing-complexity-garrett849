79c2fcd49c4eed20ca0741b75ede573e66554bd8
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-12 20:29:33 -0600

    slashes help

--- src/P2_SlashesAndBangs.java
+++ src/P2_SlashesAndBangs.java
@@ -4,8 +4,6 @@
   	Date: 
 */
 
-
-
 /* 2. Create a table below (just using text) that outlines your analysis of the figure. That is, how many
  * of each character on each line? What expression captures the relationship between the line number and
  * how many of the character is printed on each line? Choose your headings based on how you can break down
@@ -31,6 +29,21 @@ public class P2_SlashesAndBangs {
 
 	public static void main(String[] args) {
 
+		int hight = 7;
+		for (int i = 0; i <= hight; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("\\");
+			}
+			int count = (hight - i) * 2 + 1;
+			for (int k = 0; k < count; k++) {
+				System.out.print("!");
+			}
+			int back = (count + i) * 2 + 1;
+			for (int c = 0; c < back; c++) {
+				System.out.print("\\");
+			}
+			System.out.println("");
+		}
 	}
 
 }
